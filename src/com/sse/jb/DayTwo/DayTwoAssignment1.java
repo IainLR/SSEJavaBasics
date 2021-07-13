0908db3a7fa0b9e2121c08178687253e341f32fb
IainLR <iainrobertzon@gmail.com>
2021-07-13 15:23:13 -0500

    DayTwoAssignment2 refactor

--- src/com/sse/jb/DayTwo/DayTwoAssignment1.java
+++ src/com/sse/jb/DayTwo/DayTwoAssignment1.java
@@ -1,4 +1,22 @@
 package com.sse.jb.DayTwo;
 
 public class DayTwoAssignment1 {
+
+    public static void addingFromTheCommandLine(String[] args) {
+        int sum = 0;
+        StringBuilder nonNumbers = new StringBuilder();
+
+        for (String a : args) {
+            try {
+                sum += Integer.parseInt(a);
+            } catch (Exception ignored) {
+                nonNumbers.append(a).append(" ");
+            }
+        }
+
+        System.out.println("Your Sum is: " + sum);
+        System.out.println("your args which were not numbers: " + nonNumbers);
+        System.out.println();
+    }
+
 }
--- src/com/sse/jb/DayTwo/DayTwoAssignment2.java
+++ src/com/sse/jb/DayTwo/DayTwoAssignment2.java
@@ -1,4 +1,33 @@
 package com.sse.jb.DayTwo;
 
 public class DayTwoAssignment2 {
+    int[][] myMatrix = { { 0, 4, 2, 12 },
+            { 25, 5, 23, 1 },
+            { 4, 8, 1, 99 },
+            { 7, 0, 3, 11 } };
+
+    public static int maxNumberAndPositionInMatrix(int[][] matrix) {
+        int matrixRowCount = matrix.length;
+        int matrixColumnCount = matrix[0].length;
+
+        int maxNumber = Integer.MIN_VALUE;
+        int rowIndex = Integer.MIN_VALUE;
+        int columnIndex = Integer.MIN_VALUE;
+
+        for (int i = 0; i < matrixRowCount; i++) {
+            for (int l = 0; l < matrixColumnCount; l++) {
+                if (matrix[i][l] > maxNumber) {
+                    maxNumber = matrix[i][l];
+                    rowIndex = i;
+                    columnIndex = l;
+                }
+            }
+        }
+
+        System.out.printf("The largest number in this 2D Array is: %d %n", maxNumber);
+        System.out.printf("It can be found at coordinates: [%d][%d] %n", rowIndex, columnIndex);
+
+        return maxNumber;
+    }
+
 }
